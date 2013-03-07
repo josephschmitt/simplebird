9d210ef569d82c95d31fa1b2cfbdbd22b75e8b0c
Joseph Schmitt <me@joe.sh>
2013-03-07 11:53:24 -0800

    Scroll to tweets when navigating via history

--- tweets/js/simplebird.js
+++ tweets/js/simplebird.js
@@ -153,7 +153,6 @@ function drawTweetHistory() {
 	});
 
 	$('#tweet_history .bar a').click(function(e) {
-		e.preventDefault();
 		var var_name = $(e.target).parents('.bar').data('var-name');
 		loadHistoryFromVarName(var_name);
 	});
