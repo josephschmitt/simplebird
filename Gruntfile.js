f345a01e0a7f8c097a10d1ab22ccc0d89b087cb6
Joseph Schmitt <joe@vimeo.com>
2013-03-11 18:07:18 -0700

    History button tweaks

--- Gruntfile.js
+++ Gruntfile.js
@@ -54,5 +54,5 @@ module.exports = function(grunt) {
 	grunt.loadNpmTasks('grunt-contrib-watch');
 
 	// Default task(s).
-	grunt.registerTask('default', ['sass', 'uglify']);
+	grunt.registerTask('default', ['sass', 'uglify', 'watch']);
 };
